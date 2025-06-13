8cf7738b064c8c2814cf9bcce21948891999fbd7
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-12 21:07:32 -0300

    FEAT: add readChessPosition

--- src/application/UI.java
+++ src/application/UI.java
@@ -27,6 +27,18 @@ public class UI {
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 
+    public static ChessPosition readChessPosition(Scanner sc){
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        }
+        catch (RuntimeException e){
+            throw new InputMismatchException("Error reading chess position. Valid values are from a1 to h8");
+        }
+    }
+
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i=0; i<pieces.length; i++) {
             System.out.print((8 - i) + " ");
