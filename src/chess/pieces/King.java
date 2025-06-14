3751f0043b35f777b11648cde9d032671ef55ce1
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-13 23:34:11 -0300

    TEST: add a simple test in the method

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -17,6 +17,7 @@ public class King extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[0][];
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
     }
 }
