4fdbb3f0c65a2414385cf4db2f090c7395a61758
Guilherme Miranda <saguilherme16@gmail.com>
2025-07-12 19:54:17 -0300

    FEAT: fix Pawn movement logic and adjust position calculation

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -1,8 +1,8 @@
 package chess.pieces;
 
-import chess.ChessPiece;
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece {
@@ -42,7 +42,7 @@ public class Pawn extends ChessPiece {
                 mat[p.getRow()][p.getColumn()] = true;
             }
             p.setValues(position.getRow() + 2, position.getColumn());
-            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            Position p2 = new Position(position.getRow() + 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
@@ -62,4 +62,5 @@ public class Pawn extends ChessPiece {
     public String toString() {
         return "P";
     }
+
 }
